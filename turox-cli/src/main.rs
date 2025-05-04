70124967cd452fd635facee769d7b319bf6d48e4
Carson Price <75814830+cdprice02@users.noreply.github.com>
2025-05-04 15:56:06 -0600

    Init Structure (#2)
    * Update .gitignore
    
    * Add engine library and separate cli binary
    
    * Establish general engine and cli flow

---
+++ turox-cli/src/main.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+use turox_engine::Engine;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {}
+
+fn main() {
+    let args = Args::parse();
+    println!("{:?}", args);
+
+    let engine = Engine::new();
+    engine.run();
+}
---
+++ turox-engine/src/lib.rs
@@ -0,0 +1,10 @@
+#[derive(Debug, Default)]
+pub struct Engine;
+
+impl Engine {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn run(&self) {}
+}
